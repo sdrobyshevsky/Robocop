9515ff5228e80fe0a1cd08356f395982089d033b
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-04-23 08:54:28 +0300

    Added info about task2

---
+++ task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//	456 -> 6
+//	782 -> 2
+//	918 -> 8
+
+Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (number < 0)
+{
+number = -number;
+}
+int lastNumber = 0;
+if (number > 99 && number < 1000)
+{
+lastNumber = number % 10;
+Console.Write($"Последняя цифра числа {number} равнa {lastNumber}");
+Console.Write($"Последняя цифра числа {number} равнa {number % 10}");
+}
+else
+{
+Console.Write("Число не трехзначное");
+}
