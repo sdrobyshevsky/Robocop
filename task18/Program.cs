dbdf7c14a8ecfb63e6819130c6edc2379470e3ce
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-04-24 12:29:48 +0300

    Added info about Задача 13

--- task18/Program.cs
+++ task18/Program.cs
@@ -28,10 +28,10 @@ Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 string number_text = Convert.ToString(number);
 if (number_text.Length > 2)
-{
+   {
     Console.WriteLine($"Третья цифра числа - {number_text[2]}");
-}
+   }
 else
-{
+   {
    Console.WriteLine("Третьей цифры нет!");
-} 
+   } 
--- task20/Program.cs
+++ task20/Program.cs
@@ -3,3 +3,27 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+string number_text = Convert.ToString(number);
+if (number_text.Length == 5)
+{
+    if (number_text[0] == number_text[4] && number_text[1] == number_text[3]) 
+    {
+    Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+    Console.WriteLine($"{number} -  не палиндром");
+    }
+} 
+
+if (number_text.Length > 5)
+    {
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+    }  
+
+if (number_text.Length < 5)
+    {
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+    }  
