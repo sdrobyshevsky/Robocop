e36f5dfaf5177c31a208c85c05d749e57fb12802
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-04-24 13:01:31 +0300

    Added info about Задача 22

--- task22/Program.cs
+++ task22/Program.cs
@@ -2,3 +2,15 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// int sqare = 1;
+// while (sqare <= num)
+// {
+// System.Console.WriteLine(Math.Pow(sqare, 2));
+// sqare++;
+// }
+for (int i = 1; i <= num; i++)
+{
+System.Console.Write(Math.Pow(i, 3) + " ");
+} 
