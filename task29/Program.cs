6c3600ab2be7a0d5e007bdc5dff7d07bb2ac12b1
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 02:10:45 +0300

    Added info about Задача 29

--- task29/Program.cs
+++ task29/Program.cs
@@ -3,38 +3,70 @@
 // 82 -> 10
 // 9012 -> 12
 
-int number = ReadInt("Введите число: ");
-int len = NumberLen(number);
-SumNumbers(number, len);
+// Первый вариант решения задачи 
+
+// int number = ReadInt("Введите число: ");
+// int len = NumberLen(number);
+// SumNumbers(number, len);
 
 
-// Функция ввода
-int ReadInt(string message)
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// // Функция подсчета цифр в числе
+// int NumberLen(int a)
+// {
+//     int index = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         index++;
+//     }
+//     return index;
+// }
+
+// // Функция вывода суммы цифр в числе
+// void SumNumbers(int n, int len)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= len; i++)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     Console.WriteLine(sum);
+// }
+
+// Второй вариант решения задачи
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int len = Length(a);
+Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
+
+// Подсчет количества символов в числе
+int Length(int a)
 {
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (a > 0)
+{
+a /= 10;
+index++;
+}
+return index;
 }
 
-// Функция подсчета цифр в числе
-int NumberLen(int a)
+int Sum(int a, int len)
 {
-    int index = 0;
-    while (a > 0)
-    {
-        a /= 10;
-        index++;
-    }
-    return index;
+int sum = 0;
+for (int i = 1; i <= len; i++)
+{
+sum += a % 10;
+a /= 10;
 }
-
-// Функция вывода суммы цифр в числе
-void SumNumbers(int n, int len)
-{
-    int sum = 0;
-    for (int i = 1; i <= len; i++)
-    {
-        sum += n % 10;
-        n /= 10;
-    }
-    Console.WriteLine(sum);
+return sum;
 }
