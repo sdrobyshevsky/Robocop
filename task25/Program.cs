f853f957a5c6eaaf6aec6b43aae07807b61a5260
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-20 21:55:07 +0300

    Added info about Задача 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -2,3 +2,36 @@
 // b456 -> 3
 // 78 -> 2
 // 89126 -> 5
+
+int Counting(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    if (count == 0)
+    {
+        count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = A(num);
+System.Console.WriteLine($"Кол-во цифр в числе {num} = {count}");
+
+// Второй вариант решения
+
+System.Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+if (int.TryParse(num, out int number))
+{
+    System.Console.WriteLine(num.Length);
+}
+else
+{
+    System.Console.WriteLine("Ошибка: нужно ввести число");
+}
--- task26/Program.cs
+++ task26/Program.cs
@@ -1,3 +1,18 @@
 ﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4 -> 24 
 // 5 -> 120
+
+int Multiply(int number)
+{
+    int mult = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        mult *= i;
+    }
+    return mult;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int mult = Multiply(num);
+System.Console.WriteLine($"Сумма от 1 до {num} = {mult}");
