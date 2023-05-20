c4694988ca573cbc41661f03a4d29c94a34f926c
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-20 10:48:50 +0300

    Added info about Задача 25

---
+++ task24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Sum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(num);
+System.Console.WriteLine($"Сумма от 1 до {num} = {sum}");
