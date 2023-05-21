f288ccf278c44ebf662c153382c0fd9ad27e4c7f
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 12:17:50 +0300

    Added info about Задача 35

--- task35/Program.cs
+++ task35/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int newLength = array.Length / 2 + array.Length % 2;
+int[] newArray = new int[newLength];
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    newArray[i] = array[i] * array[array.Length - i - 1];
+}
+if (array.Length % 2 == 1)
+{
+    newArray[newArray.Length - 1] = array[array.Length / 2];
+}
+System.Console.WriteLine(string.Join(", ", newArray));
