1f5cbe9a67096e493fc91bfe32da5dcfe654607d
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 10:27:33 +0300

    Added info about Задача 31

--- task28/Program.cs
+++ task28/Program.cs
@@ -26,19 +26,19 @@
 
 // Второй вариант решения задачи 
 
-// Console.Write("Введите число A: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число B: ");
-// int b = Convert.ToInt32(Console.ReadLine());
-// int c = 1;
-// if (b<1)
-// Console.Write("Число B должно быть натуральным\n");
-// else
-// {
-// while (b!=0)
-// {
-// c = c * a;
-// b = b - 1;
-// }
-// Console.Write($"Число A в натуральной степени B равно {c}");
-// }
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+if (b<1)
+Console.Write("Число B должно быть натуральным\n");
+else
+{
+while (b!=0)
+{
+c = c * a;
+b = b - 1;
+}
+Console.Write($"Число A в натуральной степени B равно {c}");
+}
