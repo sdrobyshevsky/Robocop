100fec7c3621027bcb22395c4d41c8f02e9958a9
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 11:38:06 +0300

    Added info about Задача 26

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
+
+int[] arr = new int[8];
+Random rand = new Random(); // Random - является типом переменной
+// int(Random) a(rand) = 8(new Random());// double string bool - всё является типом
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rand.Next(0, 2);
+// Console.Write($"{arr[i]} ");
+}
+Console.WriteLine('[' + string.Join(", ", arr) + ']');
