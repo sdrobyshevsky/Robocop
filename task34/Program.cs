1369ee3f39d910c504c6dc47d1e319f77e97944e
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 11:58:51 +0300

    Added info about Задача 34

--- task34/Program.cs
+++ task34/Program.cs
@@ -3,3 +3,29 @@
 // [5, 18, 123, 6, 2] -> 1
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 300);
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine(count);
