40d1c0598f1b5995fb6c5bfdbc50afc40f8896cb
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 11:33:54 +0300

    Added info about Задача 33

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+int[] ChangeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = -arr[i];
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine(string.Join(", ", ChangeArray(array)));
---
+++ task33/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] FillArrayWithRandomNumber(int size)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+arr[i] = rnd.Next(-9, 10);
+}
+return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+System.Console.Write("Введите число, которое хотите найти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool isOk = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+if (number == array[i])
+{
+isOk = true;
+break;
+}
+}
+if (isOk)
+{
+System.Console.WriteLine("Элемент найден");
+}
+else
+{
+System.Console.WriteLine("Элемент не найден");
+}
