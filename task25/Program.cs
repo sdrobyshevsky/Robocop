77e134d8d368bd7062aafe90a38b0e7eec2e6ca0
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-21 20:48:03 +0300

    Added info about task 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -3,30 +3,13 @@
 // 78 -> 2
 // 89126 -> 5
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-int len = Length(a);
-Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int count = 0; //количество цифр в числе
 
-// Подсчет количества символов в числе
-int Length(int a)
+while (A > 0)
 {
-int index = 0;
-while (a > 0)
-{
-a /= 10;
-index++;
+    A /= 10; // A = A/10
+    count++;
 }
-return index;
-}
-
-int Sum(int a, int len)
-{
-int sum = 0;
-for (int i = 1; i <= len; i++)
-{
-sum += a % 10;
-a /= 10;
-}
-return sum;
-}
+Console.WriteLine($"Количество цифр в числе = {count}");
