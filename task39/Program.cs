4402537ea9bcaef2003666b5cf9d1fac9516bc1b
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-27 10:20:30 +0300

    Added info about task 39

---
+++ task39/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] FillArrayWithRandomNumber(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+
+System.Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumber(length);
+System.Console.WriteLine(string.Join(", ", array));
+
+int temp = 0;
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    temp = array[i];
+    array[i] = array[array.Length - i - 1];
+    array[array.Length - i - 1] = temp;
+}
+
+System.Console.WriteLine(string.Join(", ", array));
