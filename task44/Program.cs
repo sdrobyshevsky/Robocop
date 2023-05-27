e402a3bb08f9fdd9e62f59678ebaced479a18f85
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-27 18:34:55 +0300

    Added info about task 44

--- task44/Program.cs
+++ task44/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+void mas(int m)
+{
+for (int i = 0; i < m; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива ");
+Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+}
+
+int kol(int[] Array)
+{
+int i=0;
+int sum = 0; 
+while (i < Array.Length)
+{
+if(Array[i]>0)
+sum = sum + 1;
+i = i + 1;
+} 
+return sum;
+}
+
+mas(m);
+Console.Write($"Чисел больше нуля: {kol(Array)}");
