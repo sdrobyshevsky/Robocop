dc29afcc7d493096fbe688937f90a44bffe51d31
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-27 13:13:25 +0300

    Added info about task 41

--- task41/Program.cs
+++ task41/Program.cs
@@ -3,44 +3,58 @@
 // 3  -> 11
 // 2  -> 10
 
-int N()
+void GetBinaryView(int number)
 {
-    int NumberToInput = 0;
-    Console.Write($"Введите число: ");
-    NumberToInput = Convert.ToInt32(Console.ReadLine());
-    return NumberToInput;
+if (number == 0)
+{
+return;
 }
-void PrintArray(int[] ArrayToPrint)
-{
-    Console.Write("[");
-    for (int i = 0; i < ArrayToPrint.Length; i++)
-    {
-        Console.Write($"{ArrayToPrint[i]}");
-        if (i < ArrayToPrint.Length - 1)
-        {
-            Console.Write(" ");
-        }
-    }
-    Console.WriteLine("]");
+GetBinaryView(number / 2);
+Console.Write(number % 2);
 }
 
-int[] DecToBinConvert(int decNumberToConvert)
-{
-    int temp = decNumberToConvert;
-    int binArraySize = 0;
-    for (binArraySize = 0; temp > 0; binArraySize++)
-        temp = temp / 2;
-    int[] binArray = new int[binArraySize];
-    for (int i = binArraySize - 1; i >= 0; i--)
-    {
-        binArray[i] = decNumberToConvert % 2;
-        decNumberToConvert = decNumberToConvert / 2;
-    }
-    return binArray;
-}
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+GetBinaryView(number);
 
-int decN = N();
-int[] binNArray = DecToBinConvert(decN);
+// int N()
+// {
+//     int NumberToInput = 0;
+//     Console.Write($"Введите число: ");
+//     NumberToInput = Convert.ToInt32(Console.ReadLine());
+//     return NumberToInput;
+// }
+// void PrintArray(int[] ArrayToPrint)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < ArrayToPrint.Length; i++)
+//     {
+//         Console.Write($"{ArrayToPrint[i]}");
+//         if (i < ArrayToPrint.Length - 1)
+//         {
+//             Console.Write(" ");
+//         }
+//     }
+//     Console.WriteLine("]");
+// }
 
-Console.WriteLine("Число {0} в двоичной системе:", decN);
-PrintArray(binNArray);
+// int[] DecToBinConvert(int decNumberToConvert)
+// {
+//     int temp = decNumberToConvert;
+//     int binArraySize = 0;
+//     for (binArraySize = 0; temp > 0; binArraySize++)
+//         temp = temp / 2;
+//     int[] binArray = new int[binArraySize];
+//     for (int i = binArraySize - 1; i >= 0; i--)
+//     {
+//         binArray[i] = decNumberToConvert % 2;
+//         decNumberToConvert = decNumberToConvert / 2;
+//     }
+//     return binArray;
+// }
+
+// int decN = N();
+// int[] binNArray = DecToBinConvert(decN);
+
+// Console.WriteLine("Число {0} в двоичной системе:", decN);
+// PrintArray(binNArray);
