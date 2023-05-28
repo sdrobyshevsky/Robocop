232750dfbc687051ceafb320650c1d4d65f04931
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-28 10:08:52 +0300

    Added info about task 44

--- task44/Program.cs
+++ task44/Program.cs
@@ -2,31 +2,60 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите количество элементов массива: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[] Array = new int[m];
+// Console.Write("Введите количество элементов массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] Array = new int[m];
 
-void mas(int m)
+// void mas(int m)
+// {
+// for (int i = 0; i < m; i++)
+// {
+// Console.WriteLine($"Введите {i+1} элемент массива ");
+// Array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// }
+
+// int kol(int[] Array)
+// {
+// int i=0;
+// int sum = 0; 
+// while (i < Array.Length)
+// {
+// if(Array[i]>0)
+// sum = sum + 1;
+// i = i + 1;
+// } 
+// return sum;
+// }
+
+// mas(m);
+// Console.Write($"Чисел больше нуля: {kol(Array)}");
+
+void mas(int m, int[] Array)
 {
-for (int i = 0; i < m; i++)
-{
-Console.WriteLine($"Введите {i+1} элемент массива ");
-Array[i] = Convert.ToInt32(Console.ReadLine());
-}
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
 }
 
 int kol(int[] Array)
 {
-int i=0;
-int sum = 0; 
-while (i < Array.Length)
-{
-if(Array[i]>0)
-sum = sum + 1;
-i = i + 1;
-} 
-return sum;
+    int i = 0;
+    int sum = 0;
+    while (i < Array.Length)
+    {
+        if (Array[i] > 0)
+            sum = sum + 1;
+        i = i + 1;
+    }
+    return sum;
 }
 
-mas(m);
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+
+mas(m, Array);
 Console.Write($"Чисел больше нуля: {kol(Array)}");
