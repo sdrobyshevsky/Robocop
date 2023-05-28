3cc77a7a854dff2f01d1d3b9bc5da606225ced4c
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-28 12:18:31 +0300

    Added info about task 49

--- task50/Program.cs
+++ task50/Program.cs
@@ -3,3 +3,35 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+void FillMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+matrix[i, j] = i + j;
+}
+} 
+}
+
+void PrintMatrix(int[,] matrix)
+{
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+System.Console.Write($"{matrix[i, j]} " + "\t");
+}
+System.Console.WriteLine();
+}
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[m, n];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
