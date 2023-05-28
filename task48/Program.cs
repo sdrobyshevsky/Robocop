d8509379f49c58a3f6d2aab17d3be1b31d82ea05
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-28 11:33:25 +0300

    Added info about task 48

---
+++ task48/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Новый массив будет выглядеть 
+// вот так:
+// 1    4   49  2
+// 5    9    2    3
+// 64  4   4    4
+
+void FillMatrixWhithRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} " + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+FillMatrixWhithRandomNumbers(matrix);
+PrintMatrix(matrix);
+
+void Sqare(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {
+            matrix[i, j] = matrix[i,j] * matrix[i,j];
+        }
+    }
+}
+
+Sqare(matrix);
+System.Console.WriteLine();
+PrintMatrix(matrix);
