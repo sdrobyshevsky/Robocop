a8f403b7bfd756e996b4d9038fcb91bb5b076121
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-29 20:27:40 +0300

    Added info about task 50

--- task50/Program.cs
+++ task50/Program.cs
@@ -10,7 +10,7 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 {
 for (int j = 0; j < matrix.GetLength(1); j++)
 matrix[i, j] = Convert.ToDouble(new Random().Next(-100,1000)) / 100;
-} 
+}
 } 
 
 void PrintMatrix(double[,] matrix)
