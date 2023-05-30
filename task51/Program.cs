34121ee03c89627c1a09ac687b38cf053f58a441
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-30 23:45:31 +0300

    Added info about task 52

--- task51/Program.cs
+++ task51/Program.cs
@@ -5,46 +5,70 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-System.Console.Write("Введите количество строк массива: ");
+Console.WriteLine("Введите размеры массива");
 int m = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите количество столбцов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = new int[m, n];
-
-void FillMatrix(int[,] matrix)
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-matrix[i, j] = Convert.ToInt32(new Random().Next(-100,1000)) / 100;
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
 }
-} 
-
-void PrintMatrix(int[,] matrix)
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
 {
-for (int i = 0; i < matrix.GetLength(0); i++)
-{
-for (int j = 0; j < matrix.GetLength(1); j++)
-{
-System.Console.Write($"{matrix[i, j]} " + "\t");
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
 }
-System.Console.WriteLine();
-} 
-}
+ 
+ Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a <= m && b <= n) 
+ Console.WriteLine($"Значение элемента массива = {array[a, b]}"); 
+ else
+ {
+Console.WriteLine($"{a}{b} -> такого числа в массиве нет");
+ }
+ 
 
-System.Console.Write("Введите индекс элемента массива по строке: ");
-int i = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите индекс элемента массива по столбцу: ");
-int j = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите индекс элемента массива по строке: ");
+// int i = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите индекс элемента массива по столбцу: ");
+// int j = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = new int[3, 4]; 
 
-if (i > 0 && i < m && j > 0 && j < n)
-{  
-Console.WriteLine($"Значение элемента массива = {matrix[i, j]}");
-}
-else 
-{
-Console.WriteLine($"{i}{j} -> такого числа в массиве нет");
-}  
+// void FillMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// matrix[i, j] = Convert.ToInt32(new Random().Next(1000,10000)) / 100;
+// }
+// } 
 
-FillMatrix(matrix);
-PrintMatrix(matrix);
+// void PrintMatrix(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// System.Console.Write($"{matrix[i, j]} " + "\t");
+// }
+// System.Console.WriteLine();
+// } 
+// }
+
+// if (i >= 0 && i < matrix.GetLength(0) && j >= 0 && j < matrix.GetLength(1))
+// {  
+// Console.WriteLine($"Значение элемента массива = {matrix[i, j]}");
+// }
+// else 
+// {
+// Console.WriteLine($"{i}{j} -> такого числа в массиве нет");
+// }  
+
+// FillMatrix(matrix);
+// PrintMatrix(matrix);
