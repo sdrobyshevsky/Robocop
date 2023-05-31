5edf5ee1e37fc1c76c1b88d2e6d1f51ad444ee9f
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-31 22:40:51 +0300

    Added info about task 51

--- task51/Program.cs
+++ task51/Program.cs
@@ -8,32 +8,32 @@
 Console.WriteLine("Введите размеры массива");
 int m = Convert.ToInt32(Console.ReadLine());
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
- 
-for (int i = 0; i < array.GetLength(0); i++) 
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
-}
- 
-for (int i = 0; i < array.GetLength(0); i++) 
-{
-    for (int j = 0; j < array.GetLength(1); j++)
-        Console.Write(array[i,j] + "\t  ");
-        Console.WriteLine();
-}
- 
- Console.WriteLine("Введите координаты");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a >= 0 && a < m && b >= 0 && b < n) 
- Console.WriteLine($"Значение элемента массива = {array[a, b]}"); 
- else
- {
-Console.WriteLine($"{a}{b} -> такого числа в массиве нет");
- }
- 
+int[,] matrix = new int[m, n];
 
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+matrix[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+for (int j = 0; j < matrix.GetLength(1); j++)
+Console.Write(matrix[i, j] + "\t  ");
+Console.WriteLine();
+}
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a >= 0 && a < m && b >= 0 && b < n)
+Console.WriteLine($"Значение элемента массива = {matrix[a, b]}");
+else
+{
+Console.WriteLine($"{a}{b} -> такого числа в массиве нет");
+}
+
+  
 // System.Console.Write("Введите индекс элемента массива по строке: ");
 // int i = Convert.ToInt32(Console.ReadLine());
 // System.Console.Write("Введите индекс элемента массива по столбцу: ");
