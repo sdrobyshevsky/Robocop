00138e529c38d37dd010d5c06efd70949205dd0e
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-05-31 23:58:17 +0300

    Added info about task 52

--- task52/Program.cs
+++ task52/Program.cs
@@ -5,48 +5,31 @@
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-System.Console.Write("Введите количество строк массива: ");
+Console.WriteLine("Введите размеры массива");
 int m = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите количество столбцов массива: ");
-int n = Convert.ToInt32(Console.ReadLine()); 
-double[,] matrix = new double[m, n];
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
 
-void FillArray(double[,] array)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = Convert.ToDouble(new Random().Next(-100,1000)) / 100;
-        }
-    }
+for (int j = 0; j < matrix.GetLength(1); j++)
+matrix[i, j] = Convert.ToInt32(new Random().Next(0, 21));
 }
 
-void PrintArray(double[,] array)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
- }
-System.Console.Write("Введите индекс элемента массива по строке: ");
-int i = Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Введите индекс элемента массива по столбцу: ");
-int j = Convert.ToInt32(Console.ReadLine());
-double[] avgMatrix = new double[matrix.GetLength(1)];
-
-for (int i = 0; i < matrix.GetLength(1); i++)
-{
-    int result = 0;
-    for (int j = 0; j < matrix.GetLength(0); j++)
-    {
-        result += matrix[j, i];
-    }
-    avgMatrix[i] = result / matrix.GetLength(0);
+for (int j = 0; j < matrix.GetLength(1); j++)
+Console.Write(matrix[i, j] + "\t  ");
+Console.WriteLine();
 }
 
-PrintArray(avgMatrix); 
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.Write($"{ sum / matrix.GetLength(1)} ");
+}
+Console.ReadLine();
