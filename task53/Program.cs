2966596d37f4f96ab298519167dd795aaa4cbf36
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-03 11:03:36 +0300

    Added info about task 53

---
+++ task53/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+void FillMatrixWhithRandomNumbers(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0, 11);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} " + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SwapLinesMatrix(int[,] matrix)
+{
+    int temp = 0;
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            temp = matrix[matrix.GetLength(0)-1,j];
+            matrix[matrix.GetLength(0)-1,j] = matrix[0,j];
+            matrix[0,j] = temp;
+        }
+}
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+FillMatrixWhithRandomNumbers(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+SwapLinesMatrix(matrix);
+PrintMatrix(matrix);
+
