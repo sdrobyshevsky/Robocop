10f3bc8be533098f9bcbd56816a6bf8c390f6ce1
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-04 10:37:34 +0300

    Added info about task 61

--- task61/Program.cs
+++ task61/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+void RoadToN(int n, int m)
+{
+    if (n < m )
+    {
+        return;
+    }
+    RoadToN(n - 1, m);
+    System.Console.Write($"{n} ");
+}
+
+System.Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+RoadToN(n, m);
