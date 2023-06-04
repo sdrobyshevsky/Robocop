3880a0af574531f0ee662975a40ad46f18da8944
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-04 21:27:08 +0300

    Added info about task 66

--- task66/Program.cs
+++ task66/Program.cs
@@ -6,17 +6,9 @@ Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
- 
-SumFromMToN(m, n);
-
-// вызов функции "сумма чисел от M до N"
-void SumFromMToN(int m, int n)
-{
-    Console.Write(SumMN(m - 1, n));
-}
 
 // функция сумма чисел от M до N
-int SumMN(int m, int n)
+int SumMToN(int m, int n)
 {
     int res = m;
     if (m == n)
@@ -24,7 +16,14 @@ int SumMN(int m, int n)
     else
     {
         m++;
-        res = m + SumMN(m, n);
+        res = m + SumMToN(m, n);
         return res;
-    }
+    }    
 } 
+SumFromMToN(m, n);
+// вызов функции "сумма чисел от M до N"
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMToN(m - 1, n));
+} 
+
