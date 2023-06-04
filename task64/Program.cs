a78efec698b24666255dc9af8c8f1ad683b527f1
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-04 11:01:39 +0300

    Added info about task 64

---
+++ task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void RoadToN(int number)
+{
+if (number == 0)
+{
+return;
+}
+RoadToN(number - 1);
+System.Console.Write($"{number} ");
+
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadToN(number);
