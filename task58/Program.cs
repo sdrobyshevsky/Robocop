43c1d65f2cccbe67d2081104d3de9cd67f04258a
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-04 10:16:15 +0300

    Added info about task 57

--- task58/Program.cs
+++ task58/Program.cs
@@ -5,3 +5,70 @@
 // Результирующая матрица будет:
 // 18 20
 // 15 18
+
+System.Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] FirstMatrix = new int[m, n]; 
+int[,] SecondMatrix = new int[m, n];
+int[,] ResultMatrix = new int[m, n];
+
+FillFirstMatrix(matrix);
+PrintFirstMatrix(matrix);
+System.Console.WriteLine(); 
+ 
+FillSecondMatrix(matrix);
+PrintSecondMatrix(matrix);
+System.Console.WriteLine(); 
+
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
+        }
+    }
+}
+
+PrintArray2D(resultArray);
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция заполнения массива рандомными числами от 1 до 9
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+// Функция вывода двумерного массива в терминал 
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
