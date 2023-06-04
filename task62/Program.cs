58eefcc4f2b78d6a1ee26e6ab32cd5bd505bb172
Drobyshevsky Sergey <sdrobyshevsky@mail.ru>
2023-06-04 10:59:52 +0300

    Added info about task 62

---
+++ task62/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int SumDigital(int number)
+{
+if (number == 0)
+{
+return 0;
+}
+return number % 10 + SumDigital(number / 10);
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SumDigital(number));
